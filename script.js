0e7c6d11b8d456273d514745f05f05c27ad34ce6
Vadim Pusceddu <hankwodo@gmail.com>
2024-02-29 16:54:02 +0000

    sd

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function handleScroll() {
 }
 
 window.addEventListener("scroll", handleScroll);
+
+
+// back to yoppy
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("backToTopBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
